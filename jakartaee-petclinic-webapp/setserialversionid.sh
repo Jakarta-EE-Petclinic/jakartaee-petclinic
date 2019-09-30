2975d7f45fc1439baf23d9858f2708462294db91
ThomasWoehlke <thomas.woehlke@rub.de>
2019-09-30 15:25:15 +0200

    changed name from jakartaee8-petclinic to javaee-petclinic

--- jakartaee-petclinic-webapp/setserialversionid.sh
+++ jakartaee-petclinic-webapp/setserialversionid.sh
@@ -9,11 +9,11 @@ export CLASSPATH=
 echo "START MAVEN 1"
 ../mvnw --batch-mode --log-file=etc/mavenlog1.txt clean install -DskipTests=true -Dmaven.javadoc.skip=true
 echo "START MAVEN 2"
-../mvnw --batch-mode --log-file=etc/mavenlog2.txt dependency:tree dependency:resolve dependency:resolve-plugins dependency:unpack-dependencies -DskipTests=true -Dmaven.javadoc.skip=true
+../mvnw --batch-mode --log-file=etc/mavenlog2.txt dependency:tree dependency:resolve dependency:resolve-plugins -DskipTests=true -Dmaven.javadoc.skip=true
 echo "START MAVEN 3"
-../mvnw --batch-mode --log-file=etc/mavenlog2.txt dependency:unpack-dependencies -DskipTests=true -Dmaven.javadoc.skip=true
+../mvnw --batch-mode --log-file=etc/mavenlog3.txt dependency:unpack-dependencies -DskipTests=true -Dmaven.javadoc.skip=true
 echo "START MAVEN 4"
-../mvnw --batch-mode --log-file=etc/mavenlog3.txt dependency:build-classpath -Dmdep.outputFile=etc/classpath.txt
+../mvnw --batch-mode --log-file=etc/mavenlog4.txt dependency:build-classpath -Dmdep.outputFile=etc/classpath.txt
 echo "FINISHED MAVEN"
 export MY_CLASSPATH_DEPS=`cat etc/classpath.txt`
 export HERE=`pwd`
