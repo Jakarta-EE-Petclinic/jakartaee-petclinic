bc889fb2a0b7604ced03053a6acfe663474c2025
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-10-05 18:55:05 +0200

    move from multimodule to multi profile project

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 
 function checkDependencies() {
     TESTS_PROFILE=$1
+    BROWSER_PROFILE=$2
     SRV_PROFILES="it-default it-wildfly-managed it-wildfly-remote it-openliberty-managed it-openliberty-remote"
     for SRV_PROFILE in $SRV_PROFILES
     do
-      ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE \
+      ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE \
               clean \
               dependency:resolve \
               dependency:sources \
@@ -19,47 +20,59 @@ function checkDependencies() {
 
 function runRemoteLiberty(){
   TESTS_PROFILE=$1
+  BROWSER_PROFILE=$2
   SRV_PROFILE=it-openliberty-remote
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE clean install liberty:deploy
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE test
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE clean install liberty:deploy
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE test
 }
 
 function runRemoteWildfly(){
   TESTS_PROFILE=$1
+  BROWSER_PROFILE=$2
   SRV_PROFILE=it-wildfly-remote
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE clean install wildfly:deploy
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE test
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE clean install wildfly:deploy
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE test
 }
 
 function runManagedWildfly(){
   TESTS_PROFILE=$1
+  BROWSER_PROFILE=$2
   SRV_PROFILE=it-wildfly-managed
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE clean install wildfly:start wildfly:deploy
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE test
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE wildfly:shutdown
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE clean install wildfly:start wildfly:deploy
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE test
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE wildfly:shutdown
 }
 
 function runManagedLiberty(){
   TESTS_PROFILE=$1
+  BROWSER_PROFILE=$2
   SRV_PROFILE=it-openliberty-managed
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE clean install liberty:start liberty:deploy
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE test
-  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE liberty:stop
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE clean install liberty:start liberty:deploy
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE test
+  ./mvnw -P$SRV_PROFILE -P$TESTS_PROFILE -P$BROWSER_PROFILE liberty:stop
 }
 
 function main(){
   TESTS_PROFILE=$1
-  #checkDependencies $TESTS_PROFILE
-  runManagedWildfly $TESTS_PROFILE
-  #runRemoteWildfly $TESTS_PROFILE
-  runManagedLiberty $TESTS_PROFILE
-  #runRemoteLiberty $TESTS_PROFILE
+  BROWSER_PROFILE=$2
+  #checkDependencies $TESTS_PROFILE $BROWSER_PROFILE
+  runManagedWildfly $TESTS_PROFILE $BROWSER_PROFILE
+  #runRemoteWildfly $TESTS_PROFILE $BROWSER_PROFILE
+  runManagedLiberty $TESTS_PROFILE $BROWSER_PROFILE
+  #runRemoteLiberty $TESTS_PROFILE $BROWSER_PROFILE
 }
+
+BROWSER_PROFILE=it-browser-chrome
+#BROWSER_PROFILE=it-browser-firefox
+#BROWSER_PROFILE=it-browser-safari
+#BROWSER_PROFILE=it-browser-opera
+#BROWSER_PROFILE=it-browser-htmlunit
+#BROWSER_PROFILE=it-browser-phantomjsdriver
 
 #TESTS_PROFILE=it-skip-tests
 TESTS_PROFILE=it-run-tests
 
-main $TESTS_PROFILE
+main $TESTS_PROFILE $BROWSER_PROFILE
 
 
 
