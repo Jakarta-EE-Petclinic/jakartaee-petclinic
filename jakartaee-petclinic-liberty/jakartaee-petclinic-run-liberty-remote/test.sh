22fa53b00378c7cfb144a65279097e90bec9d145
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-10-05 13:31:46 +0200

    move from multimodule to multi profile project

--- jakartaee-petclinic-liberty/jakartaee-petclinic-run-liberty-remote/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#rm -rf org.woehlke.jakartaee.petclinic.oodm.entities.*
-../mvnw clean install
-
-
--- run.sh
+++ run.sh
@@ -1,3 +1,39 @@
 #!/usr/bin/env bash
 
-../mvnw clean install wildfly:run
+function checkDependencies() {
+    MY_PROFILES="it-default it-wildfly-managed it-wildfly-remote it-openliberty-managed it-openliberty-remote"
+    for THIS_PROFILE in $MY_PROFILES
+    do
+      ./mvnw  -P$THIS_PROFILE clean \
+              dependency:resolve \
+              dependency:sources \
+              dependency:resolve-plugins \
+              dependency:analyze \
+              dependency:analyze-dep-mgt \
+              dependency:analyze-duplicates \
+              dependency:tree
+    done
+}
+
+function deployRemoteWildfly(){
+  ./mvnw clean install wildfly:deploy && google-chrome http://localhost:8080/petclinic
+}
+
+function deployManagedWildfly(){
+  ./mvnw clean install wildfly:run -Pit-wildfly-managed && google-chrome http://localhost:8080/petclinic
+}
+
+function deployManagedLiberty(){
+  ./mvnw clean install liberty:start -Pit-wildfly-managed && google-chrome http://localhost:8080/petclinic
+}
+
+function main(){
+  #checkDependencies
+  deployRemoteWildfly
+  #deployManagedWildfly
+  #deployManagedLiberty
+}
+
+main
+
+
