1112288065197fd94cbc771824261128e6544e77
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-10-20 23:07:53 +0200

    test wildfly managed

--- server-managed-test.sh
+++ server-managed-test.sh
@@ -7,8 +7,8 @@ source src/main/bash/run-lib.bash
 function testManaged(){
   BROWSER_PROFILE=$1
   startWithMessage " TEST Managed Server"
-  testManagedWildfly $BROWSER_PROFILE
-  #testManagedLiberty $BROWSER_PROFILE
+  #testManagedWildfly $BROWSER_PROFILE
+  testManagedLiberty $BROWSER_PROFILE
   #testManagedGlassfish $BROWSER_PROFILE
   #testManagedPayara $BROWSER_PROFILE
   doneAndReady
