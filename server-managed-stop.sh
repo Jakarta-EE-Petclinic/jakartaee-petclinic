13164df199f370de3b39f80880557b882b14b7d1
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-10-20 16:03:32 +0200

    test wildfly managed

--- server-managed-stop.sh
+++ server-managed-stop.sh
@@ -7,8 +7,8 @@ source src/main/bash/run-lib.bash
 function stopManaged(){
   BROWSER_PROFILE=$1
   startWithMessage " stop Managed Server"
-  stopManagedWildfly $BROWSER_PROFILE
-  #stopManagedLiberty $BROWSER_PROFILE
+  #stopManagedWildfly $BROWSER_PROFILE
+  stopManagedLiberty $BROWSER_PROFILE
   doneAndReady
 }
 
