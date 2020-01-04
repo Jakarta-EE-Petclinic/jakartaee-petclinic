9d26ab74b4fb213125d43cdf58381797a7177434
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-01-04 12:42:17 +0100

    setup

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-#source server-managed-run.sh
-#source server-managed-stop.sh
 
+#source server-remote-start.sh
 
-source server-managed-test.sh
+source server-remote-deploy.sh
+
+#source server-remote-stop.sh
