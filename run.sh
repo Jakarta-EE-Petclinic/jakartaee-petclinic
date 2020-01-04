7d94152fb953eebda77fc7f1f2e938acb86eed10
Thomas Woehlke <thomas.woehlke@gmail.com>
2020-01-04 12:47:42 +0100

    setup

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ source server-managed-run.sh
 ##source server-managed-test.sh
 
 ##source server-remote-start.sh
-s#ource server-remote-deploy.sh
+#source server-remote-deploy.sh
 ##source server-remote-stop.sh
 
