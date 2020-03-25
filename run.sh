1f56c3f7f7ff9ebad41290ce3382c4bad92ae578
thomaswoehlke <thomas.woehlke@rub.de>
2020-03-25 17:28:33 +0100

    work in progress

--- run.sh
+++ run.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-
-#source server-managed-run.sh
-##source server-managed-stop.sh
-##source server-managed-test.sh
-
-##source server-remote-start.sh
-#source server-remote-deploy.sh
-##source server-remote-stop.sh
-
-source server-managed-dev.sh
-
-
-# https://www.ibm.com/support/knowledgecenter/SSEQTP_liberty/com.ibm.websphere.wlp.doc/ae/twlp_config_dds.html
+./mvnw
