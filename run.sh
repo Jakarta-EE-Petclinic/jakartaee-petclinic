993ea4e04c0535da6e4db96684ad2693bbc77919
thomaswoehlke <thomas.woehlke@rub.de>
2020-03-29 14:30:12 +0200

    work on issue 13

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 MY_COMMAND_1="./mvnw clean install site site:deploy liberty:run"
 MY_COMMAND_2="./mvnw"
+MY_COMMAND_2="./mvnw -Pwf-run"
 
-MY_COMMAND=MY_COMMAND_1
+MY_COMMAND=MY_COMMAND_3
 
 echo "$MY_COMMAND"
 $MY_COMMAND
