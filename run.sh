0d5c8f234349ef9a14276b5323fb8dd1bca7d26b
thomaswoehlke <thomas.woehlke@rub.de>
2020-03-30 14:36:37 +0200

    work on issue 13

--- run.sh
+++ run.sh
@@ -5,8 +5,37 @@ MY_COMMAND_2="./mvnw"
 MY_COMMAND_3="./mvnw -Pwf-run"
 MY_COMMAND_4="./mvnw -Pwlp-run"
 
-MY_COMMAND=$MY_COMMAND_4
 
-echo "$MY_COMMAND"
-$MY_COMMAND
+function dockerUp() {
+    ./mvnw docker-compose:up
+}
 
+function dockerDown() {
+    ./mvnw docker-compose:down
+}
+
+function siteAll() {
+    ./mvnw site site:deploy
+    ./mvnw -Pwf-run site site:deploy
+    ./mvnw -Pwlp-run site site:deploy
+}
+
+
+function runCommand(){
+	MY_COMMAND=$MY_COMMAND_4
+	echo "$MY_COMMAND"
+	$MY_COMMAND
+}
+
+function runAllProfiles(){
+	$MY_COMMAND_2
+	$MY_COMMAND_3
+	$MY_COMMAND_4
+}
+
+function main() {
+	runCommand
+	#runAllProfiles
+}
+
+main
